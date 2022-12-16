be3b09a8897fd9f2de9792d192d0771b0b989238
Sander ten Brinke <s.tenbrinke2@gmail.com>
2022-12-16 16:09:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using bogus_efcore;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
