81d4cc150b228fe12ee75aa71a1f280c7d052bcc
Sander ten Brinke <s.tenbrinke2@gmail.com>
2022-12-16 16:43:17 +0100

    using bogus and ef core

--- Event.cs
+++ Event.cs
@@ -4,5 +4,5 @@ public class Event
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public DateTimeOffset Date { get; set; }   
+    public DateTimeOffset Date { get; set; }
 }
